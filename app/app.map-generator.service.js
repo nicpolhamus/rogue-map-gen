c2baf74613c59ff6b299c7cff81cc159eec8488c
BagelTheKid <nicolas.polhamus@gmail.com>
2016-08-31 23:06:57 -0500

    Added documentation for map-generator service and map controller

--- app/app.map-generator.service.js
+++ app/app.map-generator.service.js
@@ -1,3 +1,7 @@
+/**
+ * Map-Generator service
+ * @namespace Services
+ */
 (function() {
   'use strict';
 
@@ -5,6 +9,12 @@
     .module('map-gen')
     .service('mapGenerator', mapGenerator);
 
+
+  /**
+   * @namespace Map-Generator
+   * @desc Map Generator that renders a Map Object
+   * @memberOf Services
+   */
   function mapGenerator() {
     var service = {
       createMap: createMap
@@ -13,23 +23,43 @@
     return service;
   }
 
+  /**
+   * Creates a Map
+   * @class Map
+   * @param {int} x Width of map
+   * @param {int} y Height of map
+   * @property {int} x    - map width
+   * @property {int} y    - map height
+   * @property {array}    - map array
+   */
   function Map(x, y) {
     this.x = x;
     this.y = y;
-    this.map = [
-    ];
+    this.map = [];
   }
-
+  
   Map.prototype = {
     populate: populate,
     seed: seeder,
     toString: toString
   };
 
+  /**
+   * @function createMap
+   * @desc Creates a Map object with x width and y height
+   * @param {int} x
+   * @param {int} y
+   * @memberof Map-Generator
+   */
   function createMap(x, y) {
     return new Map(x, y);
   }
 
+  /**
+   * @function populate
+   * @desc Populates a seeded map
+   * @memberof Map
+   */
   function populate() {
     for (var r = 0, c = 0; r < this.x-1; r++) {
       for (c = 0; c < this.y-1; c++) {
@@ -39,8 +69,20 @@
 
     //////////////
 
+    /**
+     * @function placeWall
+     * @desc Using getSurroundingWalls, determines if the current index will be a wall or not
+     * @param {int} x
+     * @param {int} y
+     * @param {object} map
+     * @memberof populate
+     */
     function placeWall(x, y, map) {
       var surroundingWalls = getSurroundingWalls(map, x, y);
+      /**
+       * this determination is called the 4/5 method
+       * it is used in cellular automata studies
+       */
       if (map.map[x][y] === 1) {
         if (surroundingWalls >= 4) {
           return 1;
@@ -55,6 +97,14 @@
 
       //////////////////////////
 
+      /**
+       * @function getSurroundingWalls
+       * @desc Checks the indecies surrounding the specific index to and returns the amount of walls
+       * @param {object} map
+       * @param {int} row
+       * @param {int} column
+       * @memberof placeWall
+       */
       function getSurroundingWalls(map, row, column) {
         var beforeX = row - 1;
         var beforeY = column - 1;
@@ -64,6 +114,11 @@
         var iY = beforeY;
         var walls = 0;
 
+        /**
+         * these for loops count around the index at [row,column]
+         * the outer loop controls the row advancement
+         * the inner loop controls the column advancement
+         */
         for (iX = beforeX; iX < afterX+1; iX++) {
           for (iY = beforeY; iY < afterY+1; iY++) {
             if (!(iX == row && iY == column)) {
@@ -76,7 +131,15 @@
         return walls;
 
         ////////////////////
-
+        
+        /**
+         * @function isWall
+         * @desc Checks to see if the specific index is a wall or not
+         * @param {object} map
+         * @param {int} x
+         * @param {int} y
+         * @memberof getSurroundingWalls
+         */
         function isWall(map, x, y) {
           if(x > -1 && y > -1) {
             console.log(map.map[x][y]);
@@ -92,6 +155,14 @@
 
           //////////////
 
+          /**
+           * @function outOfBounds
+           * @desc Checks to see if the specific index is out of bounds
+           * @param {object} map
+           * @param {int} x
+           * @param {int} y
+           * @memberof isWall
+           */
           function outOfBounds(map, x, y) {
             if (x < 0 || y < 0 || x > map.x - 2 || y > map.y - 2) {
               return true;
@@ -103,6 +174,13 @@
     }
   }
 
+  /**
+   * @function seeder
+   * @desc Returns a 2d map array that has been seeded with random 
+   * walls, except for the outside edges of the array which are always walls
+   * @param {int} percent
+   * @memberof Map
+   */
   function seeder(percent) {
     var map = [];
     var row;
@@ -131,21 +209,28 @@
 
     ////////////////
 
+    /**
+     * @function randomWall
+     * @desc Returns a wall based off the percent given
+     * @param {int} percent
+     * @memberof seeder
+     */
     function randomWall(percent) {
       return (Math.floor(Math.random() * (101 - 1) + 1) <= percent) ? 1 : 0;
     }
   }
 
+  /**
+   * @function toString
+   * @desc Returns a string representation of the map 
+   * @memberof Map
+   */
   function toString() {
-    var chars = ['.', '#', '+'];
+    var chars = ['.', '#'];
     var mapString = '';
     for (var row = 0, column = 0; row < this.x; row++) {
       for (column = 0; column < this.y; column++) {
-        if(chars[this.map[row][column]] === '.') {
-          mapString += chars[this.map[row][column]];
-        } else {
-          mapString += chars[this.map[row][column]];
-        }
+        mapString += chars[this.map[row][column]];
       }
       mapString += '\n';
     }
--- app/app.map.controller.js
+++ app/app.map.controller.js
@@ -1,3 +1,7 @@
+/**
+ * MapController controller
+ * @namespace Controllers
+ */
 (function(){
   'use strict';
 
@@ -6,6 +10,11 @@
       .controller('mapController', mapControl);
 
     mapControl.$inject = ['mapGenerator'];
+    /**
+     * @namespace MapController
+     * @desc Controls how the view renders and manipulates the Map object
+     * @memberof Controllers
+     */
     function mapControl(mapGenerator) {
       var vm = this;
       var map;
@@ -14,6 +23,11 @@
       vm.y;
       vm.perc;
 
+      /**
+       * @name generate
+       * @desc Generates Map
+       * @memberof Controllers.MapController
+       */
       function generate() {
         map =  mapGenerator.createMap(vm.y,vm.x)
         map.seed(vm.perc);
